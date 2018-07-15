4fe02be0b253c6b4f14f59aafecae3e0b362c087
AmShadows <993189197@qq.com>
2018-07-15 11:32:08 +0800

    js

--- js/image-wall.js
+++ js/image-wall.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-		           $('li.small_pic a').fancyZoom({scaleImg: true, closeOnClick: true});
-		           $('#zoom_word_1').fancyZoom({width:400, height:200});
-		           $('#zoom_word_2').fancyZoom();
-		           $('#zoom_flash').fancyZoom();
-	        });
+version https://git-lfs.github.com/spec/v1
+oid sha256:d64899bcc3343192e2ba7724c3c618ee096afa2d4293c959b2de0470250de625
+size 284
